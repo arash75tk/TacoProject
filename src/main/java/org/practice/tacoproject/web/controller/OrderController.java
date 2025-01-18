2fcdb937b41087143178d0f5f5453a1bc257b0cb
arashkaraji <arashkaraji1375@gmail.com>
2025-01-18 17:26:55 +0330

    create orderForm view

---
+++ src/main/java/org/practice/tacoproject/web/controller/OrderController.java
@@ -0,0 +1,29 @@
+package org.practice.tacoproject.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import org.practice.tacoproject.entity.TacoOrder;
+
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("tacoOrder")
+public class OrderController {
+
+    @GetMapping("/current")
+    public String orderForm() {
+        return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(TacoOrder order,
+                               SessionStatus sessionStatus) {
+        sessionStatus.setComplete();
+
+        return "redirect:/";
+    }
+
+}
