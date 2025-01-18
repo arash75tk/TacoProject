01cbbd2698eaaf3546ba314aa286ce816b081b51
arashkaraji <arashkaraji1375@gmail.com>
2025-01-18 14:27:32 +0330

    StartDev

---
+++ src/test/java/org/practice/tacoproject/TacoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.practice.tacoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
