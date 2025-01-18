8738900408a1ce64c0b6e593a3b37a650e802fe7
arashkaraji <arashkaraji1375@gmail.com>
2025-01-18 19:41:05 +0330

    change Design controller and ingredient repository

--- src/main/java/org/practice/tacoproject/repository/IngredientRepository.java
+++ src/main/java/org/practice/tacoproject/repository/IngredientRepository.java
@@ -2,8 +2,11 @@ package org.practice.tacoproject.repository;
 
 import org.practice.tacoproject.entity.Ingredient;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 import java.util.Optional;
 
 public interface IngredientRepository  extends CrudRepository<Ingredient, Long> {
     Optional<Ingredient> findById(String id);
+    List<Ingredient> findAll();
 }
--- src/main/java/org/practice/tacoproject/web/controller/DesignTacoController.java
+++ src/main/java/org/practice/tacoproject/web/controller/DesignTacoController.java
@@ -18,21 +18,11 @@ import java.util.stream.Collectors;
 @SessionAttributes("tacoOrder")
 
 public class DesignTacoController {
-
+    @Autowired
+    private IngredientRepository ingredientRepo;
     @ModelAttribute
     public void addIngredientsToModel(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP),
-                new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP),
-                new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN),
-                new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN),
-                new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES),
-                new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES),
-                new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE),
-                new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE),
-                new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE),
-                new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE)
-        );
+        List<Ingredient> ingredients = ingredientRepo.findAll();
 
         Ingredient.Type[] types = Ingredient.Type.values();
         for (Ingredient.Type type : types) {
