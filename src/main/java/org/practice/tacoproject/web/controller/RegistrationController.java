977643e0c40a54ef12d00c9c51e71ca5af503430
arashkaraji <arashkaraji1375@gmail.com>
2025-01-19 11:42:50 +0330

    config Security and login and registration Form

---
+++ src/main/java/org/practice/tacoproject/web/controller/RegistrationController.java
@@ -0,0 +1,30 @@
+package org.practice.tacoproject.web.controller;
+
+import org.practice.tacoproject.form.RegistrationForm;
+import org.practice.tacoproject.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/register")
+public class RegistrationController {
+    @Autowired
+    private UserRepository userRepo;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @GetMapping
+    public String registerForm() {
+        return "registration";
+    }
+
+    @PostMapping
+    public String processRegistration(RegistrationForm form) {
+        userRepo.save(form.toUser(passwordEncoder));
+        return "redirect:/login";
+    }
+}
